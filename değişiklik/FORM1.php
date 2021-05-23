7d32a8512747da0da3f7d556d90d5de575e2577d
nct60 <babacannecati@hotmail.com>
2021-05-23 23:17:18 +0300

    güncelleme

---
+++ değişiklik/FORM1.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+
+<html lang="tr" >
+<head>
+    <meta charset="utf-8" />
+    <title>GÝRÝÞ YAP</title>
+    <link rel="stylesheet" href="styleSheet2.css" />
+</head>
+
+
+
+
+<body>
+   
+                    <div class="login">     
+    
+                          <div class="login-screen"> 
+                                <div class="app-title"> 
+                          
+                                             <h1>GÝRÝÞ YAP </h1>
+                                        </div>
+
+                                             <div class="login-form"> 
+                                         
+                                                         <div class="control-group"> 
+                                         
+                                                                <input type="text" name="username" class="login-field" placeholder="Kullanýcý Adý id="login- name">
+                                                                <label class="login-field-icon fui user" for= "login-name"></label>
+                                                      
+                                                         </div>
+                                         
+                                                               <div class="control-group">
+                                                               
+                                                                 <input type="password" name="password" class="login-field" placeholder="sifre" id="login- pass">
+                                                                 <label class="login-field-icon fui user" for= "login-pass"></label>
+                                                              
+                                                                 </div>
+                                                                 <a href="login2.php" class="btn btn-primary btn-large  btn-block">GÝRÝÞ YAP</a>
+                                              </div>
+
+                                         </div>
+                                 </div>
+    
+    
+                          </div>
+                     </div> 
+</body>
+
+
+
+</html>
